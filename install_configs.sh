45c399462667d09d7564cb08d122261c6c90ada0
Janet Vorobyeva <janet.vorobyeva@gmail.com>
2021-02-12 09:58:35 -0500

    Added -f to install_configs

--- install_configs.sh
+++ install_configs.sh
@@ -4,12 +4,13 @@ set -eufo pipefail
 # Copies out configs to $1
 SCRIPT_DIR="$(dirname "$0")"
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -lt 1 ]; then
     echo "Needs 1 arg: home dir to copy configs to"
     exit 1
 fi
 
 HOME_DIR="$1"
+
 if [ ! -d "$HOME_DIR" ]; then
     echo "Error: '$HOME_DIR' is not a directory"
     exit 1
@@ -20,10 +21,23 @@ if [ ! -w "$HOME_DIR" ]; then
     exit 1
 fi
 
-#NOTE: wont clobber existing files
-cp -n "$SCRIPT_DIR"/.bash_aliases "$HOME_DIR/"
-cp -n "$SCRIPT_DIR"/.bashrc "$HOME_DIR/"
-cp -n "$SCRIPT_DIR"/.gitconfig "$HOME_DIR/"
-cp -n "$SCRIPT_DIR"/.gitignore_global "$HOME_DIR/"
-cp -n "$SCRIPT_DIR"/.tmux.conf "$HOME_DIR/"
-cp -n "$SCRIPT_DIR"/.vimrc "$HOME_DIR/"
+if [[ "$#" -eq "2" && "$2" == "-f" ]]; then
+    #NOTE: wont clobber existing files
+    echo OVERWRITING EXISTING CONFIGS
+    cp -f "$SCRIPT_DIR"/.bash_aliases "$HOME_DIR/"
+    cp -f "$SCRIPT_DIR"/.bashrc "$HOME_DIR/"
+    cp -f "$SCRIPT_DIR"/.gitconfig "$HOME_DIR/"
+    cp -f "$SCRIPT_DIR"/.gitignore_global "$HOME_DIR/"
+    cp -f "$SCRIPT_DIR"/.tmux.conf "$HOME_DIR/"
+    cp -f "$SCRIPT_DIR"/.vimrc "$HOME_DIR/"
+else
+    #NOTE: wont clobber existing files
+    echo Copying configs without clobbering
+    cp -n "$SCRIPT_DIR"/.bash_aliases "$HOME_DIR/"
+    cp -n "$SCRIPT_DIR"/.bashrc "$HOME_DIR/"
+    cp -n "$SCRIPT_DIR"/.gitconfig "$HOME_DIR/"
+    cp -n "$SCRIPT_DIR"/.gitignore_global "$HOME_DIR/"
+    cp -n "$SCRIPT_DIR"/.tmux.conf "$HOME_DIR/"
+    cp -n "$SCRIPT_DIR"/.vimrc "$HOME_DIR/"
+fi
+
